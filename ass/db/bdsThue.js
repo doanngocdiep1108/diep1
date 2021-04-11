f81c06eaaa278bfc57390d2d5b021fa35d112250
Doan Diep <doanngocdiep06@gmail.com>
2021-04-11 21:15:00 +0700

    first commit

---
+++ ass/db/bdsThue.js
@@ -0,0 +1,85 @@
+[
+    {
+        "Id": "T001",
+        "Name": "Cho thuê căn hộ hướng biển ",
+        "Image": "/photo/H001.jpg",
+        "Address": "Liên Chiểu, Đà Nẵng",
+        "Price": "0.5 tr/m2",
+        "S": "100 m2",
+        "People": "Nguyễn Thành Đạt",
+        "Time": "20/10/2020"
+    },
+
+    {
+        "Id": "T002",
+        "Name": "Cho thuê nhà 2 tầng ",
+        "Image": "/photo/H003.jpg",
+        "Address": "Hải Châu, Đà Nẵng",
+        "Price": "0.4 tr/m2",
+        "S": "80 m2",
+        "People": "Phan Thái Sơn",
+        "Time": "15/11/2020"
+    },
+    {
+        "Id": "T003",
+        "Name": "Cho thuê biệt thự 2 tầng",
+        "Image": "/photo/H004.jpg",
+        "Address": "Sơn Trà, Đà Nẵng",
+        "Price": "0.5 tr/m2",
+        "S": "150 m2",
+        "People": "Hoàng Thanh",
+        "Time": "25/11/2020"
+    },
+    {
+        "Id": "T004",
+        "Name": "Cho thuê nhà mặt tiền",
+        "Image": "/photo/H005.jpg",
+        "Address": "Ngũ Hành Sơn, Đà Nẵng",
+        "Price": "0.4 tr/m2",
+        "S": "140 m2",
+        "People": "Nguyễn Thu Hoài",
+        "Time": "28/11/2020"
+    },
+    {
+        "Id": "T005",
+        "Name": "Cho thuê nhà gần biển ",
+        "Image": "/photo/H006.jpg",
+        "Address": "Liên Chiểu, Đà Nẵng",
+        "Price": "0.4 tr/m2",
+        "S": "150 m2",
+        "People": "Nguyễn Thu Ngân",
+        "Time": "22/11/2020"
+    },
+
+    {
+        "Id": "T006",
+        "Name": "Cho thuê nhà mái bằng ",
+        "Image": "/photo/H009.jpg",
+        "Address": "Sơn Trà, Đà Nẵng",
+        "Price": "0.4 tr/m2",
+        "S": "180 m2",
+        "People": "Phan Mạnh",
+        "Time": "29/11/2020"
+    },
+    {
+        "Id": "B001",
+        "Name": "Bán gấp biệt thự 2 tầng giá rẻ",
+        "Image": "/photo/H002.jpg",
+        "Address": "Liên Chiểu, Đà Nẵng",
+        "Price": "50 tr/m2",
+        "S": "150 m2",
+        "People": "Nguyễn Thanh Phương",
+        "Time": "21/11/2020"
+    },
+    {
+        "Id": "B002",
+        "Name": "Bán biệt thự gần trung tâm",
+        "Image": "/photo/H008.jpg",
+        "Address": "Hải Châu, Đà Nẵng",
+        "Price": "50 tr/m2",
+        "S": "200 m2",
+        "People": "Nguyễn Hòa",
+        "Time": "25/11/2020"
+    },
+
+]
---
+++ ass/db/detail/T001.js
@@ -0,0 +1,14 @@
+[
+    {
+        "Id": "CT001",
+        "Name": "Cho thuê căn hộ hướng biển tại Đà Nẵng",
+        "Image": "/photo/H001.jpg",
+        "Address": "1112 Nguyễn Tất Thành, Hòa Minh, Liên Chiểu, Đà Nẵng",
+        "Price": "50 tr/m2",
+        "S": "100 m2",
+        "People": "Nguyễn Thành Đạt",
+        "Phone": "0987547573",
+        "Note": "Mình là chính chủ gia đình có việc đang cần bán gấp căn biệt thự mới xây 1 năm khu vực Liên Chiểu Đà Nẵng mặt tiền đường nhựa 15m. Diện tích: 10 * 30 thổ cư 100/100. Đối diện 2 trường học cấp 1, cấp 2 cách ủy ban siêu thị chợ 100m đi bộ. Giá 5.5 tỷ có ngân hàng hổ trợ - giá thương lượng. Tặng toàn bộ nội thất. Gởi 2 / 100 hoa Hồng cho người giới thiệu.",
+        "Time": "20/10/2020"
+    },
+]
